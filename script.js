5ecd6575ada4daeeacc7fa30f4d3198852ea0ec8
CoCoComputing <cory.seki@outlook.com>
2025-02-03 20:37:19 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("JavaScript is connected!");
+const button = doument.getElementById("changeColorBtn");
+button.addEventListener("click,() => {
+	document.body.syle.backgroundColor = "#ADD8E7"; // Light Blue 
+});
+
